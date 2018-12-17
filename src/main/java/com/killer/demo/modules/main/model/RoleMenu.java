5cb097ab0d3bc6ca12029fbf555fc8fede8ad019
killerWqs <1249476676@qq.com>
2018-12-17 21:16:10 +0800

    update content
    Signed-off-by: killerWqs <1249476676@qq.com>

---
+++ src/main/java/com/killer/demo/modules/main/model/RoleMenu.java
@@ -0,0 +1,45 @@
+package com.killer.demo.modules.main.model;
+
+import java.util.Date;
+
+public class RoleMenu {
+    private String roleId;
+
+    private String menuId;
+
+    private Date intime;
+
+    private Date updatetime;
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId == null ? null : roleId.trim();
+    }
+
+    public String getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(String menuId) {
+        this.menuId = menuId == null ? null : menuId.trim();
+    }
+
+    public Date getIntime() {
+        return intime;
+    }
+
+    public void setIntime(Date intime) {
+        this.intime = intime;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+}
