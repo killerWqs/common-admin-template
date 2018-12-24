34576eae379df53cc6c7e9d5c08a760748b921c3
killerWqs <killerWqs@gmail.com>
2018-12-24 16:57:07 +0800

    update content

--- im/src/main/java/com/killer/common/websocket/MyWebSocketHandler.java
+++ im/src/main/java/com/killer/common/websocket/MyWebSocketHandler.java
@@ -44,7 +44,7 @@ public class MyWebSocketHandler extends AbstractWebSocketHandler {
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        // messageType 用来区分不同的信息 身份验证 websocketsession 是不支持序列化的 所以无法存储到 redis中
+        // messageType 用来区分不同的信息 身份验证 websocketsession 是不支持序列化的 所以无法存储到 redis
         logger.info("获取到信息:" + message.getPayload().toString());
     }
 
