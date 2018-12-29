47ad7bbf683295aac17432e7d13d25d11e6f09f7
killerWqs <killerWqs@gmail.com>
2018-12-29 12:15:49 +0800

    update content

--- src/main/java/com/killer/demo/config/ApplicationConfig.java
+++ src/main/java/com/killer/demo/config/ApplicationConfig.java
@@ -1,9 +1,16 @@
 package com.killer.demo.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 
 @Configuration
 public class ApplicationConfig {
@@ -13,5 +20,43 @@ public class ApplicationConfig {
 
 //        return new HttpMessageConverters()
         return null;
+    }
+
+    // bean 注释的可以自动在参数中自动注入依赖bean StringRedisTemplate 就是使用stringRedisSerializer 作为序列化工具
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        // 设置value的序列化规则和 key的序列化规则
+        // 基本上只有一种可能了 就是不是使用该redistemplate 来存储数据的
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setDefaultSerializer(stringRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    // 需要强到一种境界，像王雨飞那样，人皆往之
+    @Bean
+    public org.springframework.session.data.redis.RedisOperationsSessionRepository redisOperationsSessionRepository (RedisTemplate redisTemplate
+                                    , ObjectMapper objectMapper) {
+        System.out.println("该bean已经被容器加载了");
+        RedisOperationsSessionRepository redisOperationsSessionRepository = new RedisOperationsSessionRepository(redisTemplate);
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        redisOperationsSessionRepository.setDefaultSerializer(jackson2JsonRedisSerializer);
+        redisOperationsSessionRepository.setDatabase(1);
+        return redisOperationsSessionRepository;
     }
 }
